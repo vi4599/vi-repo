d31e945862c66876b5af2e4bd1dec66bdb2a2c28
dev2 <dev2@mail.com>
2022-08-13 20:03:12 +0530

    modified by dev1

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
-class hello
+class hello %from dev1
 {
-	public static void main()
+	public static void main()  %from dev1
                 {
-                        system.out.print(hello dev1);
+                        system.out.print(hello dev1);  %from dev1
                 }
 }
